a1c042993a688f0b39cc363258b4b793e9781391
Antônio Irineu Filho <antonioirineu92@gmail.com>
2025-10-01 17:40:24 -0300

    feat: temporizadores nas funções do módulo fatfs

--- src/fatfs.c
+++ src/fatfs.c
@@ -36,8 +36,10 @@ void setup_spi() {
     };
     if (pico_fatfs_set_config(&cfg)) {
         printf("SPI configurado com sucesso!\n");
+        sleep_ms(3000);
     } else {
         printf("Erro ao configurar o SPI!\n");
+        sleep_ms(3000);
     }
 }
 
@@ -45,9 +47,11 @@ bool microsd_mount() {
     fr = f_mount(&fs, "", 1);
     if (fr != FR_OK) {
         printf("Erro ao montar micro SD: %d\n", fr);
+        sleep_ms(3000);
         return false;
     } else {
         printf("Micro SD montado com sucesso!\n");
+        sleep_ms(3000);
         return true;
     }
 }
@@ -56,9 +60,11 @@ bool microsd_open() {
     fr = f_open(&file, "temp.txt", FA_WRITE | FA_OPEN_APPEND);
     if (fr != FR_OK) {
         printf("Erro ao abrir arquivo temp.txt: %d\n", fr);
+        sleep_ms(3000);
         return false;
     } else {
         printf("Arquivo temp.txt aberto com sucesso!\n");
+        sleep_ms(3000);
         return true;
     }
 }
@@ -67,11 +73,13 @@ void microsd_write(const char *payload) {
     fr = f_write(&file, payload, strlen(payload), &bytes_written);
     if (fr != FR_OK) {
         printf("Erro ao gravar no arquivo temp.txt: %d\n", fr);
+        sleep_ms(3000);
     } else {
         printf("Foram gravados %u bytes em temp.txt!\n", bytes_written);
         gpio_put(LED_G, 1);
         sleep_ms(100);
         gpio_put(LED_G, 0);
+        sleep_ms(3000);
     }
     f_sync(&file);
 }
